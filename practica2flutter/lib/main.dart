d9ec31a424bfa47cff0261f66f7bd234afef7427
lorenax55 <lorenax230403@gmail.com>
2024-04-21 01:07:23 +0200

    recursividad

--- practica2flutter/lib/main.dart
+++ practica2flutter/lib/main.dart
@@ -14,8 +14,11 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Recetas App',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.amber, // Cambia a un color cálido base
         visualDensity: VisualDensity.adaptivePlatformDensity,
+        appBarTheme: AppBarTheme(
+          color: Colors.orange[300], // Color más oscuro y cálido para el AppBar
+        ),
       ),
       home: RecetasHome(),
     );
@@ -35,30 +38,49 @@ class _RecetasHomeState extends State<RecetasHome> {
 
   final List<RecetaBuilder> creadoresRecetas = [
     CroquetaRecetaBuilder(),
-    TortillaRecetaBuilder(), // Lista de creadores de builders de recetas.
+    TortillaRecetaBuilder(),
+  ];
+
+  final List<Color> colors = [
+    Colors.lightBlue[100]!,
+    Colors.lightGreen[100]!,
+    Colors.lime[100]!,
+    Colors.amber[100]!,
+    Colors.red[100]!, // Lista de colores para alternar
   ];
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Recetario'),
-      ),
-      body: Column(
-        children: [
-          Expanded(child: construirSelectorRecetas()),
-          Expanded(child: mostrarSecciones()),
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: ElevatedButton(
-              onPressed: () => aniadirNuevaSeccion(),
-              child: Text('Añadir Nueva Sección'),
+      return Scaffold(
+        appBar: AppBar(
+          title: Text('Recetario'),
+        ),
+        body: Column(
+          children: [
+            Expanded(
+              flex: 1,
+              child: construirSelectorRecetas(),
             ),
-          ),
-        ],
-      ),
-    );
+            Expanded(
+              flex: 3,
+              child: mostrarSecciones(),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: ElevatedButton(
+                onPressed: () => aniadirNuevaSeccion(),
+                style: ElevatedButton.styleFrom(
+                  backgroundColor : Color.fromARGB(255, 255, 255, 255), // Un tono más específico para este botón
+                  foregroundColor: Colors.blue, // Color del texto
+                ),
+                child: Text('Añadir Nueva Sección'),
+              ),
+            ),
+          ],
+        ),
+      );
   }
+
 
   Widget construirSelectorRecetas() {
     return ListView.builder(
@@ -74,24 +96,40 @@ class _RecetasHomeState extends State<RecetasHome> {
     );
   }
 
-  void mostrarDialogoSeccion(RecetaBuilder creador) {
-    showDialog(
-      context: context,
-      builder: (BuildContext context) {
-        return AlertDialog(
-          title: Text('Asignar Receta'),
-          content: Text('¿En qué sección deseas colocar la receta?'),
-          actions: secciones.map((seccion) => TextButton(
-            child: Text(seccion.getNombre()),
-            onPressed: () {
-              moverRecetaASeccion(creador, seccion);
-              Navigator.of(context).pop();
-            },
-          )).toList(),
-        );
+void mostrarDialogoSeccion(RecetaBuilder creador) {
+  showDialog(
+    context: context,
+    builder: (BuildContext context) {
+      return AlertDialog(
+        title: Text('Asignar Receta'),
+        content: SingleChildScrollView(
+          child: ListBody(
+            children: generarListaSecciones(secciones, creador),
+          ),
+        ),
+      );
+    },
+  );
+}
+
+List<Widget> generarListaSecciones(List<Seccion> secciones, RecetaBuilder creador, [String prefix = '']) {
+  List<Widget> list = [];
+  for (Seccion seccion in secciones) {
+    String name = prefix + seccion.getNombre();
+    list.add(TextButton(
+      child: Text(name),
+      onPressed: () {
+        moverRecetaASeccion(creador, seccion);
+        Navigator.of(context).pop();
       },
-    );
+    ));
+    if (seccion.getElementos().isNotEmpty) {
+      list.addAll(generarListaSecciones(seccion.getElementos().whereType<Seccion>().toList(), creador, '  ' + prefix));
+    }
   }
+  return list;
+}
+
 
   void moverRecetaASeccion(RecetaBuilder creador, Seccion seccion) {
     Chef chef = Chef();
@@ -109,17 +147,29 @@ class _RecetasHomeState extends State<RecetasHome> {
     );
   }
 
-  Widget crearTarjetaSeccion(Seccion seccion) {
+  Widget crearTarjetaSeccion(Seccion seccion, [int depth = 0]) {
+    Color backgroundColor = colors[depth % colors.length]; // Selecciona un color de fondo basado en la profundidad
     return Card(
+      color: backgroundColor,
+      margin: EdgeInsets.all(depth > 0 ? 16.0 : 8.0),
       child: ExpansionTile(
         title: Text(seccion.getNombre()),
         initiallyExpanded: false,
         children: [
-          for (var elemento in seccion.getElementos()) 
-            ListTile(
-              title: Text(elemento.getNombre()),
-              subtitle: Text(elemento.mostrar()),
+          for (var elemento in seccion.getElementos())
+            elemento is Seccion
+                ? crearTarjetaSeccion(elemento, depth + 1) // Incrementa la profundidad para subsecciones
+                : ListTile(
+                    title: Text(elemento.getNombre()),
+                    subtitle: Text(elemento.mostrar()),
+                  ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: ElevatedButton(
+              onPressed: () => aniadirSubseccion(seccion),
+              child: Text('Añadir Subsección'),
             ),
+          ),
         ],
       ),
     );
@@ -147,7 +197,7 @@ class _RecetasHomeState extends State<RecetasHome> {
                 setState(() {
                   secciones.add(Seccion(controladorNombreSeccion.text));
                 });
-                Navigator.of(context).pop();
+                Navigator.of(context). pop();
               },
             ),
           ],
@@ -156,5 +206,35 @@ class _RecetasHomeState extends State<RecetasHome> {
     );
   }
 
-
+  void aniadirSubseccion(Seccion parent) {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        TextEditingController controladorNombreSeccion = TextEditingController();
+        return AlertDialog(
+          title: Text('Nueva Subsección'),
+          content: TextField(
+            controller: controladorNombreSeccion,
+            decoration: InputDecoration(hintText: "Nombre de la Subsección"),
+          ),
+          actions: <Widget>[
+            TextButton(
+              child: Text('Cancelar'),
+              onPressed: () => Navigator.of(context).pop(),
+            ),
+            TextButton(
+              child: Text('Añadir'),
+              onPressed: () {
+                setState(() {
+                  Seccion newSubsection = Seccion(controladorNombreSeccion.text);
+                  parent.add(newSubsection);
+                });
+                Navigator.of(context).pop();
+              },
+            ),
+          ],
+        );
+      },
+    );
+  }
 }
