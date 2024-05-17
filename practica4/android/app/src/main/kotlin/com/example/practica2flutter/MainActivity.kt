8b4de6ed79503453ad4923f4e4d3dea682b185ed
Ignacio003 <Ignaciofortessoria@gmail.com>
2024-05-17 10:41:43 +0200

    .

--- practica4/android/app/src/main/kotlin/com/example/practica2flutter/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.practica2flutter
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
--- practica4/lib/elemento.dart
+++
@@ -1,9 +0,0 @@
-abstract class Elemento {
-  late String nombre;
-
-  void add(Elemento elemento);
-  void remove(Elemento elemento);
-  String mostrar();
-  String getNombre();
-  List<Elemento>  getElementos();
-}
